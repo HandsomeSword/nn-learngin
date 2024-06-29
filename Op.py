fb209c25e989e401a0ec9b526c6e1694b546268a
Hsword <2803343631@qq.com>
2024-06-29 23:30:51 +0800

    编写TensorOp子类中……

--- Op.py
+++ Op.py
@@ -1,8 +1,8 @@
 from abc import abstractmethod,ABC
-from typing import Tuple
+from typing import Any, Tuple
 from Value import Value
 from numpy import ndarray as NDArray
-
+from Tensor import Tensor
 class Op(ABC):
     @abstractmethod
     def compute(self,*args: Tuple["NDArray"]) -> NDArray:
@@ -14,3 +14,24 @@ class Op(ABC):
     # 参数out_grad是输出变量对应的伴随值，node是计算操作所在的计算图节点
     # 为方便编程，输出总是一个序列Tuple
         pass
+
+
+class TensorOp(Op):
+    def __call__(self, *args):
+        return Tensor.make_from_op(self, args)
+    
+
+# 对应元素相加
+class EWiseAdd(TensorOp):
+    def compute(self, a: NDArray, b: NDArray):
+        return a + b
+    def gradient(self, out_grad: Tensor, node: Tensor):
+        return out_grad, out_grad
+# 加常数
+class AddScalar(TensorOp): 
+    def __init__(self, scalar):
+        self.scalar = scalar
+    def compute(self, a: NDArray):
+        return a + self.scalar
+    def gradient(self, out_grad: Tensor, node: Tensor):
+        return (out_grad,) #重载gradient函数的输出必须是Tuple
